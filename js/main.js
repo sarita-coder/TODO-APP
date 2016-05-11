96d89ab23c27b856db3acc1320abca0032557deb
unknown <sarita-coder>
2016-05-11 10:17:32 -0500

    finalizando  todo app

--- js/main.js
+++ js/main.js
@@ -10,13 +10,19 @@ function doTweet(tweetText) {
 	
 	var elemento = document.createElement("div");
 	elemento.id="tweet";
+
 	var element_one = document.createElement("input");
 	element_one.setAttribute("type","checkbox");
-	element_one.id="tarea"
+	element_one.id="tarea";
 	element_one.onchange=function(){
-		element_two.innerHTML=tweetText.strike();
+		if(this.checked){
+			element_two.setAttribute("class","decoration");
+		} else{
+			element_two.setAttribute("class","no_decoration");
+		}
+		
 	}
-	var element_two=document.createElement("span");
+	var element_two=document.createElement("div");
 	element_two.id="description";
 	element_two.innerHTML=tweetText;
 
@@ -31,6 +37,8 @@ function doTweet(tweetText) {
 	elemento.appendChild(element_two);
 	elemento.appendChild(element_three);
 }
+
+
 function clear() {
 	var inputBox = document.getElementById("inputBox");
 	inputBox.value = "";
